3b060c7a9d8f48255156ca95e4f22f7d70823a58
Claca06 <clauscal.cc@gmail.com>
2025-01-09 10:31:13 +0100

    fixing controller

--- src/main/java/it/nesea/prenotazione_service/controller/FrontendServiceController.java
+++ src/main/java/it/nesea/prenotazione_service/controller/FrontendServiceController.java
@@ -1,23 +1,34 @@
 package it.nesea.prenotazione_service.controller;
 
 import it.nesea.albergo.common_lib.dto.response.CustomResponse;
+import it.nesea.prenotazione_service.model.Prenotazione;
 import it.nesea.prenotazione_service.model.Preventivo;
 import it.nesea.prenotazione_service.service.FrontendUtilService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/front-end-service")
 public class FrontendServiceController {
+
     private final FrontendUtilService frontendUtilService;
 
     public FrontendServiceController(FrontendUtilService frontendUtilService) {
         this.frontendUtilService = frontendUtilService;
     }
 
+    @GetMapping("/get-preventivi")
     public ResponseEntity<CustomResponse<List<Preventivo>>> getAllPreventivi() {
         return ResponseEntity.ok(CustomResponse.success(frontendUtilService.getAllPreventivi()));
+    }
+
+    @GetMapping("/get-prenotazioni")
+    public ResponseEntity<CustomResponse<List<Prenotazione>>> getAllPrenotazioni() {
+        return ResponseEntity.ok(CustomResponse.success(frontendUtilService.getAllPrenotazioni()));
     }
 
 }
