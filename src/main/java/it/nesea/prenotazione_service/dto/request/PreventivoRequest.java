747ca8616ef3b68a7f5820b34083b77b6a2e6b1b
ChristianSorace9226 <soracechristian0@gmail.com>
2025-01-16 12:24:10 +0100

    reformat code

--- src/main/java/it/nesea/prenotazione_service/dto/request/PreventivoRequest.java
+++ src/main/java/it/nesea/prenotazione_service/dto/request/PreventivoRequest.java
@@ -10,7 +10,6 @@ import lombok.experimental.FieldDefaults;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
